e67c2c751bef84c1fcb5df4759d7b76b34c6817c
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-09 18:34:13 +0100

    checking server error on cloudinary_signature

--- backend/settings.py
+++ backend/settings.py
@@ -23,7 +23,7 @@ CLOUDINARY_STORAGE = {
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 def get_list_from_config(config_name):
