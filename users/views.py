51ae66795944cae6717d7c7bf30c769dc6dffb2c
Mats Gustafsson <113367401+Moonchichiii@users.noreply.github.com>
2024-01-16 15:26:03 +0100

    Still having issues with the logout .

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import auth
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.exceptions import APIException
